6fdc0de282ca51ccbf398c346a45a5f1e5abc784
Egor Bichurin <bichurin.es@gmail.com>
2021-11-21 11:09:49 +0500

    refactor: логика закрытия модального окна перенесена в компонент Modal

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -4,20 +4,9 @@ import {  } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './modal-overlay.module.css';
 
 function ModalOverlay({ children, closeAllPopups }) {
-  const handleEscPress = (e) => {
-    e.key === 'Escape' && closeAllPopups();
-  }
-
   const handleOverlayClick = (e) => {
     e.target === e.currentTarget && closeAllPopups();
   }
-
-  React.useEffect(() => {
-    document.addEventListener('keydown', handleEscPress);
-    return () => {
-      document.removeEventListener('keydown', handleEscPress)
-    }
-  });
 
   return (
     <div className={styles.overlay} onMouseDown={handleOverlayClick}>
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
@@ -7,6 +8,17 @@ import ModalOverlay from '../modal-overlay/modal-overlay';
 const modalRoot = document.getElementById('react-modals');
 
 function Modal({ children, title, closeAllPopups }) {
+  const handleEscPress = (e) => {
+    e.key === 'Escape' && closeAllPopups();
+  }
+
+  React.useEffect(() => {
+    document.addEventListener('keydown', handleEscPress);
+    return () => {
+      document.removeEventListener('keydown', handleEscPress)
+    }
+  });
+
   return ReactDOM.createPortal(
     <ModalOverlay closeAllPopups={closeAllPopups}>
       <div className={`${styles.modal} pl-10 pt-10 pr-10`}>
